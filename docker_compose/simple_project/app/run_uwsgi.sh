e82dcbb82fcbbe87c2280d1a90a9e3c7f9e39159
josejesuscp <cp.josejesus@gmail.com>
2024-04-23 08:56:21 -0400

    added docker changes

--- docker_compose/simple_project/app/run_uwsgi.sh
+++ docker_compose/simple_project/app/run_uwsgi.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-python3 manage.py migrate --noinput
-python3 manage.py createsuperuser --no-input || true
+python3 manage.py migrate --noinput --fake
+# python3 manage.py createsuperuser --no-input || true
 python3 manage.py collectstatic --noinput
 
 set -e
 
 chown www-data:www-data /var/log
 
-uwsgi --strict --ini /etc/app/uwsgi.ini
+uwsgi --strict --ini uwsgi.ini
