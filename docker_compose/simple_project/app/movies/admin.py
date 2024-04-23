93c037f3c0e6b219de5c2a9ad300844762b62854
cpjosejesus <cp.josejesus@gmail.com>
2024-04-23 00:19:33 -0400

    changed docker compose

--- docker_compose/simple_project/app/movies/admin.py
+++ docker_compose/simple_project/app/movies/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Genre, GenreFilmWork
+from .models import FilmWork
+from .models import Person, PersonFilmWork
 
-# Register your models here.
+
+class GenreFilmworkInline(admin.TabularInline):
+    model = GenreFilmWork
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created', 'modified')
+    list_filter = ('name',)
+    search_fields = ('name', 'id')
+
+
+@admin.register(FilmWork)
+class FilmworkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmworkInline,)
+    list_display = ('title', 'type', 'creation_date',
+                    'rating', 'created', 'modified')
+    list_filter = ('type',)
+    search_fields = ('title', 'description', 'id')
+
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmWork
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    inlines = (PersonFilmworkInline,)
+    list_display = ('full_name', 'created', 'modified')
+    list_filter = ('full_name',)
+    search_fields = ('full_name',)
--- docker_compose/simple_project/app/run_uwsgi.sh
+++ docker_compose/simple_project/app/run_uwsgi.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+python3 manage.py migrate --noinput
+python3 manage.py createsuperuser --no-input || true
+python3 manage.py collectstatic --noinput
+
 set -e
 
 chown www-data:www-data /var/log
