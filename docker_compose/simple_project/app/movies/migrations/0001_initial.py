d0f475b58b68eae779d12e1ebd3b38b390043295
cpjosejesus <cp.josejesus@gmail.com>
2024-04-23 21:10:21 -0400

    review1

--- docker_compose/simple_project/app/movies/migrations/0001_initial.py
+++ docker_compose/simple_project/app/movies/migrations/0001_initial.py
@@ -13,6 +13,11 @@ class Migration(migrations.Migration):
     dependencies = []
 
     operations = [
+        migrations.RunSQL(
+                   sql="""
+                       CREATE SCHEMA IF NOT EXISTS content;
+                       """
+               ),
         migrations.CreateModel(
             name="Filmwork",
             fields=[
--- docker_compose/simple_project/app/run_uwsgi.sh
+++ docker_compose/simple_project/app/run_uwsgi.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 python3 manage.py migrate --noinput --fake
-# python3 manage.py createsuperuser --no-input || true
+python3 manage.py createsuperuser --no-input || true
 python3 manage.py collectstatic --noinput
 
 set -e
